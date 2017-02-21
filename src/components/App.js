356f39808e14079702e6399b33504ab9e6edf30d
alex rodriguez <alex.rodriguez@austintexas.gov>
2017-02-21 13:02:03 -0600

    add sass middleware for express

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+
+  componentDidMount () {
+    // ajax fetching would go here.
+    // timers, listeners
+  }
+
+  componentWillUnmount () {
+    // clean timers and listeners
+  }
+  
+  
+
   render () {
     return (
       <div className="container">
-        <Header message='hi header' />
-        <h2>the app</h2>
+        <Header message={this.state.pageHeader} />
+        <div className="Contest">
+          {this.props.contests.map(contest => <ContestPreview {...contest} /> )}
+        </div>
       </div>
     );
   }
