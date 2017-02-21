1acaf09421c12e9800f30e8ff3f6d84ece11a6d0
alex rodriguez <alex.rodriguez@austintexas.gov>
2017-02-21 13:16:23 -0600

    add axios and call data from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
   state = {
-    pageHeader: 'Naming Contests'
+    pageHeader: 'Naming Contests',
+    contests: []
   };
 
   componentDidMount () {
     // ajax fetching would go here.
     // timers, listeners
+    axios.get('/api/contests')
+      .then(data => {
+        // console.log(data.data.contests);
+        this.setState({
+          contests: data.data.contests
+        });
+      })
+      .catch(console.error);
+
+    // this.setState({
+    //   contests: data.contests
+    // });
   }
 
   componentWillUnmount () {
@@ -24,7 +38,7 @@ class App extends React.Component {
       <div className="container">
         <Header message={this.state.pageHeader} />
         <div className="Contest">
-          {this.props.contests.map(contest => <ContestPreview {...contest} /> )}
+          {this.state.contests.map(contest => <ContestPreview key={contest.id} {...contest} /> )}
         </div>
       </div>
     );
